a8521a23701722778e64269370952e7c1ce8ea3e
Mohamed <mmmoussa@uwaterloo.ca>
2015-10-16 21:50:05 -0400

    Removed fuzzy search from processing algorithm

--- src/processResults.py
+++ src/processResults.py
@@ -27,72 +27,65 @@ def processText(value, skip=False):
 		else:
 			processText(value, skip=True) # Restart call ignoring initial results
 	else:
-		print "No matches. Trying fuzzy search."
-		fuzzyAyahs = alfanous.do({"action": "search", "query": value, "fuzzy": "true"})["search"]["ayas"]
-		if len(fuzzyAyahs) > 0:
-			matched = getMatchItem(fuzzyAyahs[1]) # 1 is number given for top match by alfanous, not list index
-			printResults(matched)
-			return responseJSON(value, matched)
+		print "No matches. Trying spaces."
+		spaceAyahs = []
+		spaces = [space.start() for space in re.finditer(' ', value)]
+		for space in spaces:
+			spacedValue = value[:space] + value[(space+1):]
+			spacedAyahs = alfanous.do({"action": "search", "query": spacedValue})["search"]["ayas"]
+			if len(spacedAyahs) > 0:
+				spacedMatched = getMatchItem(spacedAyahs[1])
+				spaceAyahs.append(spacedMatched)
+		if len(spaceAyahs) > 0:
+			mostCommonMatch = mostCommon(value.encode("utf-8"), spaceAyahs)
+			printResults(mostCommonMatch)
+			return responseJSON(value, mostCommonMatch)
 		else:
-			print "No matches. Trying spaces."
-			spaceAyahs = []
-			spaces = [space.start() for space in re.finditer(' ', value)]
-			for space in spaces:
-				spacedValue = value[:space] + value[(space+1):]
-				spacedAyahs = alfanous.do({"action": "search", "query": spacedValue})["search"]["ayas"]
-				if len(spacedAyahs) > 0:
-					spacedMatched = getMatchItem(spacedAyahs[1])
-					spaceAyahs.append(spacedMatched)
-			if len(spaceAyahs) > 0:
-				mostCommonMatch = mostCommon(value.encode("utf-8"), spaceAyahs)
+			print "No matches. Trying suggestions."
+			suggestionAyahs = []
+			suggestionsObj = alfanous.do({"action": "suggest", "query": value})["suggest"]
+			suggestions = {}
+			for a in suggestionsObj:
+				suggestions[a] = []
+				b = suggestionsObj[a]
+				for c in b:
+					suggestions[a].append(c)
+			
+			for i in suggestions:
+				for j in suggestions[i]:
+					newValue = value.replace(i, j)
+					# print i
+					# print j
+					# print newValue
+					newAyahs = alfanous.do({"action": "search", "query": newValue})["search"]["ayas"]
+					if len(newAyahs) > 0:
+						newMatched = getMatchItem(newAyahs[1])
+						suggestionAyahs.append(newMatched)
+			if len(suggestionAyahs) > 0:
+				mostCommonMatch = mostCommon(value.encode("utf-8"), suggestionAyahs)
 				printResults(mostCommonMatch)
 				return responseJSON(value, mostCommonMatch)
 			else:
-				print "No matches. Trying suggestions."
-				suggestionAyahs = []
-				suggestionsObj = alfanous.do({"action": "suggest", "query": value})["suggest"]
-				suggestions = {}
-				for a in suggestionsObj:
-					suggestions[a] = []
-					b = suggestionsObj[a]
-					for c in b:
-						suggestions[a].append(c)
-				
+				print "No matches. Trying spaces and suggestions."
+				ssAyahs = []
 				for i in suggestions:
 					for j in suggestions[i]:
 						newValue = value.replace(i, j)
-						# print i
-						# print j
-						# print newValue
-						newAyahs = alfanous.do({"action": "search", "query": newValue})["search"]["ayas"]
-						if len(newAyahs) > 0:
-							newMatched = getMatchItem(newAyahs[1])
-							suggestionAyahs.append(newMatched)
-				if len(suggestionAyahs) > 0:
-					mostCommonMatch = mostCommon(value.encode("utf-8"), suggestionAyahs)
+						spaces = [space.start() for space in re.finditer(' ', newValue)]
+						for space in spaces:
+							ssValue = newValue[:space] + newValue[(space+1):]
+							newAyahs = alfanous.do({"action": "search", "query": ssValue})["search"]["ayas"]
+							if len(newAyahs) > 0:
+								ssMatched = getMatchItem(newAyahs[1])
+								ssAyahs.append(ssMatched)
+
+				if len(ssAyahs) > 0:
+					mostCommonMatch = mostCommon(value.encode("utf-8"), ssAyahs)
 					printResults(mostCommonMatch)
 					return responseJSON(value, mostCommonMatch)
 				else:
-					print "No matches. Trying spaces and suggestions."
-					ssAyahs = []
-					for i in suggestions:
-						for j in suggestions[i]:
-							newValue = value.replace(i, j)
-							spaces = [space.start() for space in re.finditer(' ', newValue)]
-							for space in spaces:
-								ssValue = newValue[:space] + newValue[(space+1):]
-								newAyahs = alfanous.do({"action": "search", "query": ssValue})["search"]["ayas"]
-								if len(newAyahs) > 0:
-									ssMatched = getMatchItem(newAyahs[1])
-									ssAyahs.append(ssMatched)
-
-					if len(ssAyahs) > 0:
-						mostCommonMatch = mostCommon(value.encode("utf-8"), ssAyahs)
-						printResults(mostCommonMatch)
-						return responseJSON(value, mostCommonMatch)
-					else:
-						print "No matches at all."
-						return responseJSON(value, {}, empty=True)
+					print "No matches at all."
+					return responseJSON(value, {}, empty=True)
 
 def getMatchItem(ayah):
 	matchItem = {
